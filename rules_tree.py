0ef3fe88f01e2c626ae6accac9f02fe02c573261
Ashara Shrestha <aashara.shrestha@gmail.com>
2019-11-23 15:21:44 -0600

    trailing braces and apostrophes handled

--- rules_tree.py
+++ rules_tree.py
@@ -1,39 +1,43 @@
 import pandas as pd
 from treelib import Node, Tree
 
-df_rules = pd.read_csv("Rules.csv", dtype=str)
+df_rules = pd.read_csv("/Users/aasharashrestha/Documents/PycharmProjects/SeasonalTrends/Seasonality_Project/Paper_5/Project/VersionControl/ModifiedApriori/Rules.csv", dtype=str)
 
 df_rules['LHS'] = df_rules.LHS.str.replace('[', '')
 df_rules['LHS'] = df_rules.LHS.str.replace(']', '')
+df_rules['LHS'] = df_rules.LHS.str.replace('\'', '')
 
-lhs = '\'2_Emergency\''
-rhs = '\'3_Not Discharged\''
-
-tree = Tree()
-tree.create_node(lhs+"->"+rhs, "root")  # root node
-
-my_queue = []
-
+rules_len_2_set = set()
 for index, row in df_rules.iterrows():
-    left_rule = lhs +', ' + rhs
-    if(row['LHS']== left_rule):
-        left = row['LHS']
-        right = row['RHS']
-        a = left + ", "+ "\'"+ right + "\'"
-        tree.create_node(left +"->"+ right + "[Conf:" + row["Conf"] + "]", a, parent="root")
-        my_queue.append(left + ", \'"+ right + "\'")
+    lst = []
 
-while len(my_queue)!=0:
-    elem = my_queue.pop(0)
+    lhs = row['LHS']
+    rhs = row['RHS']
+
+
+    tree = Tree()
+    tree.create_node(lhs+"->"+rhs + "[Conf: "+ row["Conf"]+"]", "root")  # root node
+
+    my_queue = []
+
     for index, row in df_rules.iterrows():
-        if(row['LHS'] == elem):
+        left_rule = lhs +', ' + rhs
+        if(row['LHS']== left_rule):
             left = row['LHS']
             right = row['RHS']
-            a = left + ", " + "\'" + right + "\'"
-            tree.create_node(left + "->" + right + "[Conf:" + row["Conf"] + "]", a, parent= left)
-            my_queue.append(left + ", \'" + right + "\'")
+            a = left + ", " + right
+            tree.create_node(left +"->"+ right + "[Conf:" + row["Conf"] + "]", a, parent="root")
+            my_queue.append(left + ", "+ right)
 
+    while len(my_queue)!=0:
+        elem = my_queue.pop(0)
+        for index, row in df_rules.iterrows():
+            if(row['LHS'] == elem):
+                left = row['LHS']
+                right = row['RHS']
+                a = left + ", " + right # concatenating lhs and rhs to find the result in lhs of dataframe
+                tree.create_node(left + "->" + right + "[Conf:" + row["Conf"] + "]", a, parent= left)
+                my_queue.append(left + ", " + right)
 
-
-tree.show(line_type="ascii-em")
+    tree.show(line_type="ascii-em")
 

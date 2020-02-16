eec9294b17e964a91eb33fc334b2680b3df0f9bb
ashara <ashara.shrestha@gmail.com>
2020-02-16 17:59:57 -0600

    GUI for viewing rules tree.

--- GUI.py
+++ GUI.py
@@ -1,134 +1,156 @@
-from tkinter import *
+
+import tkinter as tk
 import pandas as pd
 from treelib import Node, Tree
-from time import time
-import datetime
-import rules_tree
-
-t0 = time()
-
-start = datetime.datetime.now()
-print("Started at: ", start)
-
-#method to show tree dumped in the file after createRule gets completed.
-def showTree():
-    f = open('tree.txt', 'r')
-    text = ""
-    for i in range(10):
-        contents = f.read()
-        # t = list(contents)
-        # break
-        text += contents
-    txt = Label( text=text, justify=LEFT)
-    f.close()
-    txt.pack()
-# #Method to create rule tree:
-# def createRulesTree():
-#     rules_tree.main("1_a")
+import os
 
 
-def createRulesTree(filter=None):
-    df_rules = pd.read_csv("Rules.csv", dtype=str)
+class SampleApp(tk.Tk):
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
+        self.om_variable = tk.StringVar()
+        ageChoices  = self.use_age()
+        LOSChoices = self.use_LOS()
+        DXChoices = self.use_DX()
+        HACChoices = self.use_HAC()
 
-    df_rules['LHS'] = df_rules.LHS.str.replace('[', '')
-    df_rules['LHS'] = df_rules.LHS.str.replace(']', '')
-    df_rules['LHS'] = df_rules.LHS.str.replace('\'', '')
+        self.om_variable.set("SELECT")
+        self.om_variable.trace("w", self.setfilter)
+        self.filter = None
 
-    rules_len_2_set = set()
+        b1 = tk.Button(self, text="Age", width=12, command=lambda: self.changeOption(ageChoices))
+        b2 = tk.Button(self, text="LOS", width=12, command=lambda: self.changeOption(LOSChoices))
+        b3 = tk.Button(self, text="Primary DX", width=12, command=lambda: self.changeOption(DXChoices))
+        b4 = tk.Button(self, text="HAC Septicemia", width=12, command=lambda: self.changeOption(HACChoices))
+        b5 = tk.Button(self, text="Show Rules", width=12, command=self.createRulesTree)
 
-    #filter df for LHS with the argument given by user.
-    if filter!=None:
-        df_rules = df_rules[df_rules['LHS'].str.contains(filter)]
+        self.om = tk.OptionMenu(self, self.om_variable, *ageChoices)
+        self.om.configure(width=20)
 
-    for index, row in df_rules.iterrows():
-        lhs_split = []
-        lhs = row['LHS']
-        rhs = row['RHS']
-        if ',' not in lhs: # keeping such rules as rules where # of elem in lhs and rhs is 1.
-            tree = Tree()
-            tree.create_node(lhs+"->"+rhs + "[Conf: "+ row["Conf"]+"]", "root")  # root node
+        b1.pack(side="left")
+        b2.pack(side="left")
+        b3.pack(side="left")
+        b4.pack(side="left")
+        b5.pack(side="left")
+        self.om.pack(side="left", fill="x", expand=True)
 
-            my_queue = []
+    def changeOption(self,choices):
+        self._reset_option_menu(choices, 0)
 
-            for index, row in df_rules.iterrows():
-                left_rule = lhs +', ' + rhs
-                if(row['LHS']== left_rule):
-                    left = row['LHS']
-                    right = row['RHS']
-                    a = left + ", " + right
-                    tree.create_node(left +"->"+ right + "[Conf:" + row["Conf"] + "]", a, parent="root")
-                    my_queue.append(left + ", "+ right)
 
-            while len(my_queue)!=0:
-                elem = my_queue.pop(0)
+    def setfilter(self,*args):
+        self.filter = self.om_variable.get()
+        print(self.om_variable.get())
+
+    def createRulesTree(self):
+        '''
+
+        :param
+        :return:
+        '''
+        print(self.filter)
+        df_rules = pd.read_csv("Rules.csv", dtype=str)
+
+        df_rules['LHS'] = df_rules.LHS.str.replace('[', '')
+        df_rules['LHS'] = df_rules.LHS.str.replace(']', '')
+        df_rules['LHS'] = df_rules.LHS.str.replace('\'', '')
+
+        # filter df for LHS with the argument given by user.
+        if self.filter != None:
+            df_rules = df_rules[df_rules['LHS'].str.contains(self.filter)]
+            df_rules = df_rules[df_rules['LHS'].str.contains(self.filter)]
+
+        for index, row in df_rules.iterrows():
+            lhs = row['LHS']
+            rhs = row['RHS']
+            if ',' not in lhs:  # keeping such rules as rules where # of elem in lhs and rhs is 1.
+                tree = Tree()
+                tree.create_node(lhs + "->" + rhs + "[Conf: " + row["Conf"] + "]", "root")  # root node
+
+                my_queue = []
+
                 for index, row in df_rules.iterrows():
-                    if(row['LHS'] == elem):
+                    left_rule = lhs + ', ' + rhs
+                    if (row['LHS'] == left_rule):
                         left = row['LHS']
                         right = row['RHS']
-                        a = left + ", " + right # concatenating lhs and rhs to find the result in lhs of dataframe
-                        tree.create_node(left + "->" + right + "[Conf:" + row["Conf"] + "]", a, parent= left)
+                        a = left + ", " + right
+                        tree.create_node(left + "->" + right + "[Conf:" + row["Conf"] + "]", a, parent="root")
                         my_queue.append(left + ", " + right)
 
-            tree.show(line_type="ascii-em")
-            tree.save2file('tree.txt',line_type='ascii-em')
-            showTree()
+                while len(my_queue) != 0:
+                    elem = my_queue.pop(0)
+                    for index, row in df_rules.iterrows():
+                        if (row['LHS'] == elem):
+                            left = row['LHS']
+                            right = row['RHS']
+                            a = left + ", " + right  # concatenating lhs and rhs to find the result in lhs of dataframe
+                            tree.create_node(left + "->" + right + "[Conf:" + row["Conf"] + "]", a, parent=left)
+                            my_queue.append(left + ", " + right)
 
-#It starts by creating a Tk object and pass it to a tkinter frame created with Frame()
-root = Tk()
-root.title("Tk dropdown example")
-mainframe = Frame(root)
+                tree.show(line_type="ascii-em")
+                if os.path.exists("tree.txt"):
+                    os.remove("tree.txt")
 
-#A grid is added to the frame which will hold the combo-box.
-mainframe.grid(column=0,row=0, sticky=(N,W,E,S) )
-mainframe.columnconfigure(0, weight = 1)
-mainframe.rowconfigure(0, weight = 1)
-mainframe.pack(pady = 100, padx = 100)
+                tree.save2file('tree.txt', line_type='ascii-em')
+                self.showTree()
 
-#The popup menu contains a list of options which is defined in the variable choices.
-# A Tkinter variable is created with the line:
-tkvar = StringVar(root)
+    def showTree(self):
+        f = open('tree.txt', 'r')
+        text = ""
+        for i in range(10):
+            contents = f.read()
+            text += contents
+        txt = tk.Label(text=text, justify="left")
+        f.close()
+        txt.pack()
 
-# Dictionary with options
-filepath = "/Users/ashara/Documents/Data/Scenario1_Top10CCS.csv"
-df = pd.read_csv(filepath)
-tkvar.set('Select') # set the default option
+    def _reset_option_menu(self, options, index=None):
+        '''reset the values in the option menu
 
-#The default value of the variable is set with the .set() method.
-#We create the Tkinter combobox with:
+        if index is given, set the value of the menu to
+        the option at the given index
+        '''
+        menu = self.om["menu"]
+        menu.delete(0, "end")
+        for string in options:
+            menu.add_command(label=string,
+                             command=lambda value=string:
+                                  self.om_variable.set(value))
 
-#dropdown for Age Group
-choices = set(df.AGE_GROUP.unique())
-popupMenu = OptionMenu(mainframe, tkvar, *choices)
-Label(mainframe, text="Choose an event").grid(row = 1, column = 1)
-popupMenu.grid(column =1)
+        if index is not None:
+            self.om_variable.set(options[index])
 
-# #dropdown for LOS
-# choices = set(df.PRIMARY_DX.unique())
-# popupMenu_1 = OptionMenu(mainframe, tkvar, *choices)
-# popupMenu_1.grid(column =1)
-#
-# #dropdown for PRIMARY_DX
-# choices = set(df.PRIMARY_DX.unique())
-# popupMenu_2 = OptionMenu(mainframe, tkvar, *choices)
-# popupMenu_2.grid(column =1)
-#
-# #dropdown for DEVELOPED_SEPTICEMIA
-# choices = set(df.DEVELOPED_SEPTICEMIA.unique())
-# popupMenu_3 = OptionMenu(mainframe, tkvar, *choices)
-# popupMenu_3.grid(row = 5, column =1)
+    def use_age(self):
+        '''Switch the option menu to display Age'''
+        filepath = "/Users/ashara/Documents/Data/Scenario1_Top10CCS.csv"
+        df = pd.read_csv(filepath)
+        choices = list(set(df.AGE_GROUP.unique()))
+        return choices
 
 
-#Buttont0 = time()
+    def use_LOS(self):
+        '''Switch the option menu to display LOS'''
+        filepath = "/Users/ashara/Documents/Data/Scenario1_Top10CCS.csv"
+        df = pd.read_csv(filepath)
+        choices = list(set(df.LOS.unique()))
+        return choices
 
-start = datetime.datetime.now()
-print("Started at: ", start)
-print("Value from popmenu: ", mainframe.getvar() tkvar.get())
-Btn1 = Button(root, text="Show Rule Tree",  command=lambda : createRulesTree(popupMenu))
-# xbBrowse = Button(frameN, text="Browse...", font=fontReg, command=lambda : createRulesTree())
+    def use_DX(self):
+        '''Switch the option menu to display LOS'''
+        filepath = "/Users/ashara/Documents/Data/Scenario1_Top10CCS.csv"
+        df = pd.read_csv(filepath)
+        choices = list(set(df.PRIMARY_DX.unique()))
+        return choices
 
-Btn1.pack()
+    def use_HAC(self):
+        '''Switch the option menu to display LOS'''
+        filepath = "/Users/ashara/Documents/Data/Scenario1_Top10CCS.csv"
+        df = pd.read_csv(filepath)
+        choices = list(set(df.DEVELOPED_SEPTICEMIA.unique()))
+        return choices
 
-#if event chosen then filter the LHS by this event.
 
-mainframe.mainloop()
-
+if __name__ == "__main__":
+    app = SampleApp()
+    app.mainloop()
